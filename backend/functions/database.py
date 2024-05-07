8126b961dd38c612a40cd4c83d276ff4929ac5a0
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2024-05-06 23:20:06 -0600

    update messages functions

--- backend/functions/database.py
+++ backend/functions/database.py
@@ -44,11 +44,24 @@ def get_recent_messages():
     return messages
 
 # Store Messages
-def store_messages():
+def store_messages(request_message, response_message):
     # Define the file name
     file_name = "stored_data.json"
 
     # Get recent messages
     messages = get_recent_messages()[1:]
 
-    
+    # Add messages to data
+    user_message = {"role": "user", "content": request_message}
+    assistant_message = {"role": "assistant", "content": response_message}
+    messages.append(user_message)
+    messages.append(assistant_message)
+
+    # Save the updated file
+    with open(file_name, "w") as f:
+        json.dump(messages, f)
+
+# Reset messages
+def reset_messages():
+    # Overwrite current file
+    open("stored_data.json", "w")
--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 import openai
+from functions.database import store_messages, reset_messages
 from functions.openai_requests import convert_audio_to_text, get_chat_response
 
 app = FastAPI()
@@ -33,6 +34,12 @@ app.add_middleware(
 async def check_health():
     return {"message": "Healthy"}
 
+# Reset Messages
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()
+    return {"message": "Conversation reset"}
+
 # GET Audio
 @app.get("/post-audio-get")
 async def get_audio():
@@ -50,6 +57,9 @@ async def get_audio():
     
     # Get chat response
     chat_response = get_chat_response(message_decoded)
+
+    # Store messages
+    store_messages(message_decoded, chat_response)
 
     print(chat_response)
     return chat_response
