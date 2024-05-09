2f83e5d75812d7adbaf842f0f12998213e45af39
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2024-05-08 20:29:48 -0600

    Update chat style

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -60,9 +60,9 @@ const Controller = () => {
     return(
         <div className='h-screen overflow-y-hidden'>
             <Title setMessages={setMessages} />
-            <div className='flex flex-col justify-between h-full overflow-y-scroll pb-96'>
+            <div className='w-full  justify-between h-full overflow-y-scroll pb-96'>
                 {/* Conversation */}
-                <div className="mt-5 px-5">
+                <div className="mt-5 px-5 max-w-7xl mx-auto flex flex-col">
                     {messages.map((audio, index) => {
                         return (
                             <div 
@@ -94,10 +94,8 @@ const Controller = () => {
                     )}
                 </div>
                 {/* Recorder */}
-                <div className='fixed bottom-0 w-full py-5 border-t text-center bg-gradient-to-r from-[#915EFF] to-sky-300'>
-                    <div className='flex justify-center items-center w-full'>
-                        <RecordMessage handleStop={handleStop} />
-                    </div>
+                <div className='fixed bottom-0 w-full py-5 text-center bg-gradient-to-l from-[#915EFF] to-sky-900'>
+                    <RecordMessage handleStop={handleStop} />
                 </div>
             </div>
         </div>
--- frontend/src/components/RecordIcon.tsx
+++ frontend/src/components/RecordIcon.tsx
@@ -11,7 +11,7 @@ function RecordIcon({classText}: Props) {
             viewBox="0 0 24 24" 
             strokeWidth={1.5} 
             stroke="currentColor" 
-            className={"w-12 h-12 " + classText}
+            className={"w-12 h-12 sm:w-8 sm:h-8 " + classText}
         >
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
         </svg>
